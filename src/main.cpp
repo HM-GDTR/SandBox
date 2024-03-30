ace4564301e67fc95fac92b1907a913dcb0abed1
HM-GDTR <yan3260325524@outlook.com>
2024-03-30 15:03:12 +0800

    添加日志记录器

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,54 @@
+// 标准库头文件
 #include <string>
 #include <iostream>
 
-#include "types.hpp"
-#include "SandBoxConfig.h"
+#include <boost/asio.hpp>
+
+// 第三方库头文件
+#include "json.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include "tclap/CmdLine.h"
 
-int main (int argc, char *argv[], char *envp[]) {
-    TCLAP::CmdLine parser("A code SandBox for Online Judge", ' ', SandBox_VERSION);
-    TCLAP::ValueArg<std::string> pipeName("p", "pipe", "Name of the pipeline used to transmit information", false, "", "string");
-    parser.add(pipeName);
+#include "types.hpp"
+#include "SandBoxConfig.h"
+
+using json = nlohmann::json;
+using boost::asio::ip::tcp;
+
+void loggerInit() {
+    auto stderr_logger = spdlog::stderr_color_mt("sandbox");
+    auto file_logger = spdlog::basic_logger_mt("basic_logger", "latest.log");
+
+    spdlog::set_default_logger(stderr_logger);
+    spdlog::get("stderr")->sinks().push_back(file_logger->sinks().front());
+}
+
+void registerCmdLine(int argc, char *argv[]) {
+    using namespace TCLAP;
+
+    CmdLine parser("A code SandBox for Online Judge", ' ', SandBox_VERSION);
+    ValueArg<int> portName("p", "port", "Specify port of remote connects", false, 49999, "number");
+    ValueArg<std::string> hostName("H", "host", "Specify host of remote connects", false, "127.0.0.1", "dns name or ip address");
+    SwitchArg verbose("v", "verbose", "Enable showing the complex detail");
+
+    parser.add(portName);
+    parser.add(hostName);
+    parser.add(verbose);
+
     parser.parse(argc, argv);
 
+    if(verbose) {
+        spdlog::set_level(spdlog::level::trace);
+    } else {
+        spdlog::set_level(spdlog::level::info);
+    }
+}
 
+int main (int argc, char *argv[], char *envp[]) {
+    loggerInit();
+    registerCmdLine(argc, argv);
 
     return 0;
 }
